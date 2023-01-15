08bf30cd514e4c7ccf7300b084870554f4707287
AlbinaKuleta <116918514+AlbinaKuleta@users.noreply.github.com>
2023-01-15 22:57:25 +0100

    Update activities.js

--- activities.js
+++ activities.js
@@ -1,4 +1,3 @@
-
 const textareaEl = document.getElementById("textarea")
 const totalCounterEl = document.getElementById ("total-counter")
 const remainingCounterEl = document.getElementById ("remaining-counter");
@@ -39,3 +38,45 @@ function updateImage(){
 }
 
 
+let btn = document.querySelector('.btn');
+let clip = document.querySelector('.clip');
+let close= document.querySelector('.close');
+let video= document.querySelector('video');
+btn.onclick = function(){
+    btn.classList.add('active')
+    clip.classList.add('active')
+    video.play();
+}
+close.onclick = function(){
+    btn.classList.remove('active')
+    clip.classList.remove('active')
+
+}
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
