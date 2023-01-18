629fc46f6d3b95e518c716d475bc010e3ac9fe39
Ferdeze Nurkasa <116812415+FerdezeNurkasa@users.noreply.github.com>
2023-01-18 15:50:33 +0100

    js

---
+++ aboutus.js
@@ -0,0 +1,36 @@
+//DOM elements
+const time = document.getElementById('time'),
+  greeting = document.getElementById('greeting')
+
+
+// Options
+const showAmPm = true;
+
+// Show Time
+function showTime() {
+  let today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds();
+
+  // Set AM or PM
+  const amPm = hour >= 12 ? 'PM' : 'AM';
+
+
+  // Output Time
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
+    sec
+  )} ${showAmPm ? amPm : ''}`;
+
+  setTimeout(showTime, 1000);
+}
+
+// Add Zeros
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+
+
+// Run
+showTime();
