c1065847820b9848b82147c42e8700dbda14e255
ajsheselmani <ajshe.selmani@student.uni-pr.edu>
2023-01-18 15:41:23 +0100

    some other changes

--- Rpage2.js
+++ Rpage2.js
@@ -1,7 +1,8 @@
+//for gallery slideshow
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next and previous controls
+// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -14,7 +15,8 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -25,12 +27,8 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 } 
 
-<div class="rating" style="width: 20rem">
-    <input id="rating-5" type="radio" name="rating" value="5"/><label for="rating-5"><i class="fas fa-3x fa-star"></i></label>
-    <input id="rating-4" type="radio" name="rating" value="4" checked /><label for="rating-4"><i class="fas fa-3x fa-star"></i></label>
-    <input id="rating-3" type="radio" name="rating" value="3"/><label for="rating-3"><i class="fas fa-3x fa-star"></i></label>
-    <input id="rating-2" type="radio" name="rating" value="2"/><label for="rating-2"><i class="fas fa-3x fa-star"></i></label>
-    <input id="rating-1" type="radio" name="rating" value="1"/><label for="rating-1"><i class="fas fa-3x fa-star"></i></label>
-</div>
+
+
