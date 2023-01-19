30fade6d682548160f6bf66cc6b83078ede8d76b
AlbinaKuleta <116918514+AlbinaKuleta@users.noreply.github.com>
2023-01-19 22:46:05 +0100

    changes

--- activities.js
+++ activities.js
@@ -53,18 +53,3 @@ close.onclick = function(){
     clip.classList.remove('active')
 
 }
-
-
-function allowDrop(ev) {
-    ev.preventDefault();
-  }
-  
-  function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-  }
-  
-  function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-  }
